b8114ad290619e003eaedbb2544ce75e50952194
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-12-13 10:10:18 -0500

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,24 +1,42 @@
-#!/usr/bin/python3
-"""Script to generete request using a given APIs"""
+#/!usr/bin/python3
+"""
+This module start the conecction with API jsonplace
+"""
 import requests
 from sys import argv
+import csv
 
+
+def gather():
+    """
+    This methos return the tasks of the users
+    """
+
+    url_all = "https://jsonplaceholder.typicode.com/todos?"
+    url_user = "https://jsonplaceholder.typicode.com/users?"
+    argv_all = {'userId': argv[1]}
+    argv_user = {'id': argv[1]}
+
+    response_all = requests.get(url_all, params=argv_all)
+    response_user = requests.get(url_user, params=argv_user)
+
+    all_json = response_all.json()
+    user_json = response_user.json()
+
+    name = user_json[0]['username']
+    id = user_json[0]['id']
+    list_date = []
+    
+    for date in all_json:
+        dates = f'{id},{name},{date["completed"]},{date["title"]}'
+        list_date.append(dates)
+    for date in list_date:
+        print(date)
+    
+    with open('2.csv', 'w', encoding='UTF8') as file:
+        writer = csv.writer(file)
+        
+        writer.writerow(list_date)
 
 if __name__ == '__main__':
-    """Script for task0"""
-    user_request = requests.get(
-        'http://jsonplaceholder.typicode.com/users/{}'.format(argv[1])).json()
-    todos_req = requests.get(
-        'http://jsonplaceholder.typicode.com/todos').json()
-    csvs = ""
-    name = str(user_request.get('username'))
-    uid = argv[1]
-    for task in todos_req:
-        if task.get('userId') == int(argv[1]):
-            csvs += ("\"{}\",\"{}\",\"{}\",\"{}\"\n"
-                     .format(uid,
-                             name,
-                             task.get('completed'),
-                             task.get('title')))
-    with open(("{}.csv").format(uid), "w", encoding="utf-8") as f:
-        f.write(csvs)Gather data from an API
+    gather()
